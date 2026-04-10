28d468c5b822603a575d7ee4055bebcc1cc6f6bb
Carolina Bezerra <carolina.soofer@gmail.com>
2026-04-10 10:23:04 -0300

    feat: cria classe Filme com atributos, atores e diretor

--- film-system/src/models/Filme.java
+++ film-system/src/models/Filme.java
@@ -1,5 +1,45 @@
 package models;
 
-// entidade que representa um filme no catálogo, ele deve possuir um diretor e uma lista de atores
+import java.util.ArrayList;
+import java.util.List;
+
 public class Filme {
+	private String nome;
+	private String dataLancamento;
+	private double orcamento;
+	private String descricao;
+	
+	private Diretor diretor;
+	private List<Ator> atores;
+	
+	public Filme(String nome, String dataLancamento, double orcamento, String descricao) {
+		this.nome = nome;
+		this.dataLancamento = dataLancamento;
+		this.orcamento = orcamento;
+		this.descricao = descricao;
+		this.atores = new ArrayList<>();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setDiretor(Diretor diretor) {
+		this.diretor = diretor;
+	}
+	
+	public void adicionarAtor(Ator ator) {
+		this.atores.add(ator);
+	}
+	
+	public String exibirDetalhes() {
+		String nomeDiretor = (diretor != null) ? diretor.getNome() : "Diretor não definido";
+		
+		return "Filme: " + nome +
+				"\nData de lançamento: " + dataLancamento +
+				"\nOrçamento: " + orcamento +
+				"\nDescrição: " + descricao +
+				"\n\nDiretor: " + nomeDiretor +
+				"\nQuantidade de atores: " + atores.size();
+	}
 }
